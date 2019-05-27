b32edf7f13c9a110e6f5fa34850ee92519d87e41
f <f@sutty.nl>
2019-05-27 17:28:15 -0300

    purgecss: no procesar a menos que estemos en producción

--- _plugins/purgecss.rb
+++ _plugins/purgecss.rb
@@ -1,6 +1,8 @@
 # frozen_string_literal: true
 
 Jekyll::Hooks.register :site, :post_write do |site|
+  next unless ENV['JEKYLL_ENV'] == 'production'
+
   system('./node_modules/.bin/purgecss ' \
          "--css #{site.dest}/#{site.config.dig('css')} " \
          "--content #{site.dest}/**/*.html #{site.dest}/*.html " \
