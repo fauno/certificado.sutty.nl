a277412aad79edc1c7a021d4eab7e26137782069
f <f@sutty.nl>
2019-06-19 16:11:05 -0300

    desplegar el menú responsive con js

--- assets/js/sutty.js
+++ assets/js/sutty.js
@@ -2,3 +2,17 @@
 ---
 
 {% turbolinks %}
+
+{% comment %}
+TODO: cuando minifiquemos JS, solo incorporar zepto y event.js
+{% endcomment %}
+{% node_module zepto/dist/zepto.min.js %}
+
+$(document).on('click', '.navbar-burger', function(e) {
+  e.preventDefault();
+
+  var _toggler = $(e.currentTarget);
+  var _navbar  = _toggler.parents('nav').find('.navbar-menu');
+
+  _navbar.toggleClass('is-active');
+});
